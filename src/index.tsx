751ec96f6de753a58482142a3139e7189172f037
CyberHD1811 <63724420+CyberHD1811@users.noreply.github.com>
2023-06-08 00:27:06 +0200

    Version 1.1.6
    Fixing issue with module exports

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@
 
 import { render } from 'solid-js/web';
 
-import App, { MainContext as MC } from './App';
+import App, { MainContext } from './App';
 
 import './styles/index.scss';
 
 render(() => <App />, document.getElementById('root')!);
 
-export type NumberPair = [number, number];
-export const MainContext = MC;
+type NumberPair = [number, number];
+
+export { MainContext };
+export type { NumberPair };
